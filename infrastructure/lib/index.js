9a712d8a6f39ecb532a9d0a11e708258c6546621
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-27 20:25:19 -0500

    Setting up our Serverless infrastructure

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -1,7 +1,12 @@
-import MyStack from "./MyStack";
+import S3Stack from "./S3Stack";
+import CognitoStack from "./CognitoStack";
+import DynamoDBStack from "./DynamoDBStack";
 
+// Add stacks
 export default function main(app) {
-  new MyStack(app, "my-stack");
+  new DynamoDBStack(app, "dynamodb");
 
-  // Add more stacks
+  const s3 = new S3Stack(app, "s3");
+
+  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
 }
